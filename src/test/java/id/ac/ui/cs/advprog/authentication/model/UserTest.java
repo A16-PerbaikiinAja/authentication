98afcbe2ac229cd5b54d061f2b0da7ac1187fb47
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:31:26 +0700

    [RED] Add skeleton for User model unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/authentication/model/UserTest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.authentication.model;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @Test
+    void testUserGettersAndSetters() {
+    }
+
+    @Test
+    void testUserToString() {
+    }
+}
