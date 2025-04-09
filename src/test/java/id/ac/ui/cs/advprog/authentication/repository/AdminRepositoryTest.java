f3381923581a235721f2b7e2a7bc7e059e02c2f9
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:44:26 +0700

    [RED] Add tests for Admin repository

--- src/test/java/id/ac/ui/cs/advprog/authentication/repository/AdminRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/authentication/repository/AdminRepositoryTest.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
+import id.ac.ui.cs.advprog.authentication.model.Admin;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class AdminRepositoryTest {
@@ -12,5 +15,11 @@ class AdminRepositoryTest {
 
     @Test
     void testFindByEmail() {
+        Admin admin = new Admin("Admin Name", "admin@example.com", "1234567890", "hashedPassword");
+        adminRepository.save(admin);
+
+        Optional<Admin> found = adminRepository.findByEmail("admin@example.com");
+        assertTrue(found.isPresent());
+        assertEquals("Admin Name", found.get().getFullName());
     }
 }
