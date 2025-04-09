8c073672f8703b7304c9d59dbc813c098c631ecd
Fazil <faziltirtana@tutanota.com>
2025-04-09 23:07:01 +0700

    [RED] Add skeleton for Auth Response DTO

---
+++ src/main/java/id/ac/ui/cs/advprog/authentication/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.authentication.dto;
+
+import lombok.Data;
+import lombok.Generated;
+
+@Generated
+@Data
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+}
