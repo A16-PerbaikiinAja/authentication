59b6f2dea3e2f583c1fef53b079d6f20cd5e35a2
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:52:18 +0700

    [RED] Add tests for Technician Repository

--- src/test/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepositoryTest.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
+import id.ac.ui.cs.advprog.authentication.model.Technician;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class TechnicianRepositoryTest {
@@ -12,5 +15,11 @@ class TechnicianRepositoryTest {
 
     @Test
     void testFindByEmail() {
+        Technician tech = new Technician("Tech Name", "tech@example.com", "0987654321", "hashedPassword", 5, "Address", 0, 0.0);
+        technicianRepository.save(tech);
+
+        Optional<Technician> found = technicianRepository.findByEmail("tech@example.com");
+        assertTrue(found.isPresent());
+        assertEquals("Tech Name", found.get().getFullName());
     }
 }
