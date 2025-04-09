da8c7e79103eaaee663e1180a1af3d19dad6c078
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:28:51 +0700

    [RED] Add skeleton for Technician model unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/authentication/model/TechnicianTest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.authentication.model;
+
+import org.junit.jupiter.api.Test;
+
+class TechnicianTest {
+
+    @Test
+    void testTechnicianGettersAndSetters() {
+    }
+
+    @Test
+    void testTechnicianToString() {
+    }
+}
