27a566ab638666aab4df4230c1da0d97f91232df
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:53:26 +0700

    [GREEN] Implement Technician Repository

--- src/main/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepository.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
+import id.ac.ui.cs.advprog.authentication.model.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import id.ac.ui.cs.advprog.authentication.model.Technician;
+import java.util.Optional;
 
 @Repository
 public interface TechnicianRepository extends JpaRepository<Technician, Long> {
+    Optional<Technician> findByEmail(String email);
 }
