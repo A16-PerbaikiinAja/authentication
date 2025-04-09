6f36d326f4b952c88af73ef26f51c35177669e06
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:46:51 +0700

    [GREEN] Implement Admin Repository

--- src/main/java/id/ac/ui/cs/advprog/authentication/repository/AdminRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/repository/AdminRepository.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
+import id.ac.ui.cs.advprog.authentication.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import id.ac.ui.cs.advprog.authentication.model.Admin;
+import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
 }
