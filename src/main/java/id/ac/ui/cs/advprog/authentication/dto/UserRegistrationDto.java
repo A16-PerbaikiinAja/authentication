23acb2c2146826fdef56fd8e7134dbb18e58374b
Fazil <faziltirtana@tutanota.com>
2025-04-09 23:13:07 +0700

    [GREEN] Implement User Registration DTO

--- src/main/java/id/ac/ui/cs/advprog/authentication/dto/UserRegistrationDto.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/dto/UserRegistrationDto.java
@@ -6,4 +6,9 @@ import lombok.Generated;
 @Generated
 @Data
 public class UserRegistrationDto {
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String address;
 }
