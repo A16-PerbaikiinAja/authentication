7b2202c505144a8d3d09864d764651618b832beb
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:48:09 +0700

    [RED] Add skeleton for DataLoader configuration unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/authentication/config/DataLoaderTest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.authentication.config;
+
+import org.junit.jupiter.api.Test;
+
+class DataLoaderTest {
+
+    @Test
+    void testLoadAdmin_CreatesAdminIfNoneExists() throws Exception {
+    }
+
+    @Test
+    void testLoadAdmin_DoesNotCreateAdminIfExists() throws Exception {
+    }
+}
