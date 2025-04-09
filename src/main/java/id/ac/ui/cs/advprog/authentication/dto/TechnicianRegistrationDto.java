067e4a65463d06aa73c2d1cb3e1aaa437646ce94
Fazil <faziltirtana@tutanota.com>
2025-04-09 23:12:29 +0700

    [GREEN] Implement Technician Registration DTO

--- src/main/java/id/ac/ui/cs/advprog/authentication/dto/TechnicianRegistrationDto.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/dto/TechnicianRegistrationDto.java
@@ -6,4 +6,10 @@ import lombok.Generated;
 @Generated
 @Data
 public class TechnicianRegistrationDto {
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private Integer experience;
+    private String address;
 }
