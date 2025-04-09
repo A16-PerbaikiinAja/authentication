d147de9f89e8bd568ce2f3963cab8b20ac237d7b
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:54:43 +0700

    [RED] Add tests for User Repository

--- src/test/java/id/ac/ui/cs/advprog/authentication/repository/UserRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/authentication/repository/UserRepositoryTest.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
+import id.ac.ui.cs.advprog.authentication.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class UserRepositoryTest {
@@ -12,5 +15,11 @@ class UserRepositoryTest {
 
     @Test
     void testFindByEmail() {
+        User user = new User("User Name", "user@example.com", "1112223333", "hashedPassword", "Address");
+        userRepository.save(user);
+
+        Optional<User> found = userRepository.findByEmail("user@example.com");
+        assertTrue(found.isPresent());
+        assertEquals("User Name", found.get().getFullName());
     }
 }
