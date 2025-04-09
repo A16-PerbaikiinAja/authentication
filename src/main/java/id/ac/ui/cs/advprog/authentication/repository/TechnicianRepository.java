b6b754ff42ac6b148e47a3d16463cefae00ed1a8
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:52:59 +0700

    [RED] Add Technician Repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepository.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.authentication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.authentication.model.Technician;
+
+@Repository
+public interface TechnicianRepository extends JpaRepository<Technician, Long> {
+}
