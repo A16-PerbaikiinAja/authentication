a606327aa8c9d6926187108f8c06b55677f1553f
Fazil <faziltirtana@tutanota.com>
2025-04-09 23:39:21 +0700

    [GREEN] Implement Authentication Controller

--- src/main/java/id/ac/ui/cs/advprog/authentication/controller/AuthenticationController.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/controller/AuthenticationController.java
@@ -1,26 +1,51 @@
 package id.ac.ui.cs.advprog.authentication.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-
+import jakarta.annotation.security.PermitAll;
 import id.ac.ui.cs.advprog.authentication.dto.AuthRequest;
+import id.ac.ui.cs.advprog.authentication.dto.AuthResponse;
 import id.ac.ui.cs.advprog.authentication.dto.TechnicianRegistrationDto;
 import id.ac.ui.cs.advprog.authentication.dto.UserRegistrationDto;
+import id.ac.ui.cs.advprog.authentication.model.Technician;
+import id.ac.ui.cs.advprog.authentication.model.User;
 import id.ac.ui.cs.advprog.authentication.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+    @Autowired
     public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
     }
 
+    @PermitAll
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        try {
+            AuthResponse response = authenticationService.login(authRequest);
+            return ResponseEntity.ok(response);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
     }
 
+    @PermitAll
+    @PostMapping("/register/user")
     public ResponseEntity<?> registerUser(@RequestBody UserRegistrationDto registrationDto) {
+        User user = authenticationService.registerUser(registrationDto);
+        return ResponseEntity.ok(user);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/register/technician")
     public ResponseEntity<?> registerTechnician(@RequestBody TechnicianRegistrationDto registrationDto) {
+        Technician technician = authenticationService.registerTechnician(registrationDto);
+        return ResponseEntity.ok(technician);
     }
 }
