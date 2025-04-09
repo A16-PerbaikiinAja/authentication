7aef798cba7efcd695e41fc5223f17d7ef42a41c
Fazil <faziltirtana@tutanota.com>
2025-04-09 22:54:24 +0700

    [RED] Add skeleton for User Repository unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/authentication/repository/UserRepositoryTest.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.authentication.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testFindByEmail() {
+    }
+}
