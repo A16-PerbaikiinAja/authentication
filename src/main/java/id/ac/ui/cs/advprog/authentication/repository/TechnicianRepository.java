69306d912affbd8ff7e87d992169dfdc53909166
Fazil <faziltirtana@tutanota.com>
2025-04-10 15:19:57 +0700

    [REFACTOR] Change primary key type from Long to UUID in repository interfaces

--- src/main/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/repository/TechnicianRepository.java
@@ -1,11 +1,14 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
-import id.ac.ui.cs.advprog.authentication.model.Technician;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import id.ac.ui.cs.advprog.authentication.model.Technician;
 
 @Repository
-public interface TechnicianRepository extends JpaRepository<Technician, Long> {
+public interface TechnicianRepository extends JpaRepository<Technician, UUID> {
     Optional<Technician> findByEmail(String email);
 }
--- src/main/java/id/ac/ui/cs/advprog/authentication/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package id.ac.ui.cs.advprog.authentication.repository;
 
-import id.ac.ui.cs.advprog.authentication.model.User;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import id.ac.ui.cs.advprog.authentication.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
